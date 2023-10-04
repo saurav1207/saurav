1fed362c7a9c5e3fb2e45d8d10f36a040f8d530f
saurav1207 <sauravkumar843114@gmail.com>
2023-10-05 02:27:47 +0530

    Updated code

--- src/components/Service.js
+++ src/components/Service.js
@@ -11,19 +11,19 @@ const service = () => {
         <i class="fa-solid fa-laptop-code"></i>
           <h2>Software Engg.</h2>
           <p>Providing expert software engineering services to craft robust and scalable solutions tailored precisely to your requirements. Let's build together.</p>
-          <a href="#">Learn more</a>
+          {/* <a href="#service">Learn more</a> */}
         </div>
         <div>
         <i className="fa-solid fa-code"></i>
           <h2>Web Develop</h2>
           <p>Offering creative web development solutions that breathe life into your online presence, captivating your audience and delivering an exceptional user experience.</p>
-          <a href="#">Learn more</a>
+          {/* <a href="#service">Learn more</a> */}
         </div>
         <div>
         <i class="fa-solid fa-crop-simple"></i>
           <h2>Web Designing</h2>
           <p>Crafting comprehensive web design solutions that tackle complex challenges and optimize your workflow. Elevate your digital presence with our expertise.</p>
-          <a href="#">Learn more</a>
+          {/* <a href="#service">Learn more</a> */}
         </div>
       </div>
 
