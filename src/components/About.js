747004ec185b8a5f8c3f9ea08d354b3cfcde2a5e
saurav1207 <sauravkumar843114@gmail.com>
2023-10-22 21:57:32 +0530

    added image

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/About.css'; 
-import saurya from '../assets/saurya.jpg';
+import saurya from '../assets/saurya.png';
 
 const About = () => {
   function opentab(tabname, event) {
